d1ea5f26cd3886ac7ece69169e55ceed1fc241cd
gledinburgh <gledinburgh@gmail.com>
2022-04-01 08:37:12 -0500

    added new petshop assets

--- Components/ModalContent/PetShopContent.tsx
+++ Components/ModalContent/PetShopContent.tsx
@@ -7,15 +7,22 @@ const PetShopContent = () => {
 
 
 
+    // <Row className={styles["scene"]} justify="center" align="middle">
+    //   <Col id={styles['terrarium-container']}>
+    //     <img id={styles["terrarium-back"]} style={{ width: "100%" }} src='./mockup/terrarium-back.png'></img>
+    //     <img id={styles["terrarium-front"]} style={{ width: "100%" }} src='./mockup/terrarium-front.png'></img>
+    //     <img id={styles["coming-soon"]} src='./mockup/coming-soon.png' />
+    //     <Col id={styles["hermit-crab-translate"]}>
+    //       <img id={styles["hermit-crab"]} src='./mockup/hermit1.png' />
+    //     </Col>
+
+    //   </Col>
+
+    // </Row>
+
     <Row className={styles["scene"]} justify="center" align="middle">
       <Col id={styles['terrarium-container']}>
-        <img id={styles["terrarium-back"]} style={{ width: "100%" }} src='./mockup/terrarium-back.png'></img>
-        <img id={styles["terrarium-front"]} style={{ width: "100%" }} src='./mockup/terrarium-front.png'></img>
-        <img id={styles["coming-soon"]} src='./mockup/coming-soon.png' />
-        <Col id={styles["hermit-crab-translate"]}>
-          <img id={styles["hermit-crab"]} src='./mockup/hermit-crab.png' />
-        </Col>
-
+        <img id={styles["terrarium-back"]} style={{ width: "100%" }} src='./mockup/petshop.png' />
       </Col>
 
     </Row>
