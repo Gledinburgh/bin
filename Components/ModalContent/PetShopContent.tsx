8abd0248329a0bffcc29f0d2ed2db82de6d57e94
gledinburgh <gledinburgh@gmail.com>
2022-04-01 11:47:29 -0500

    added parallex

--- Components/ModalContent/PetShopContent.tsx
+++ Components/ModalContent/PetShopContent.tsx
@@ -1,28 +1,57 @@
 import { Row, Col } from 'antd'
-import styles from '../../styles/Modal.module.css'
+import Parallax from 'parallax-js'
+import { useEffect, useRef } from 'react'
+
+import styles from '../../styles/PetShop.module.css'
+
+const depths = {
+  door: "2",
+  pedistal: ".52",
+  terrarium: ".5",
+  hermit: ".4",
+  register: ".25",
+  counter: ".2",
+  back: ".05",
+}
 
 const PetShopContent = () => {
+
+  const sceneEl = useRef(null)
+
+  useEffect(() => {
+    const parallaxInstance = new Parallax(sceneEl.current, {
+      // relativeInput: true,
+      hoverOnly: true
+    })
+
+    parallaxInstance.enable();
+
+    return () => parallaxInstance.disable();
+  }, [])
+
   return (
 
 
-
-
-    // <Row className={styles["scene"]} justify="center" align="middle">
-    //   <Col id={styles['terrarium-container']}>
-    //     <img id={styles["terrarium-back"]} style={{ width: "100%" }} src='./mockup/terrarium-back.png'></img>
-    //     <img id={styles["terrarium-front"]} style={{ width: "100%" }} src='./mockup/terrarium-front.png'></img>
-    //     <img id={styles["coming-soon"]} src='./mockup/coming-soon.png' />
-    //     <Col id={styles["hermit-crab-translate"]}>
-    //       <img id={styles["hermit-crab"]} src='./mockup/hermit1.png' />
-    //     </Col>
-
-    //   </Col>
-
-    // </Row>
-
     <Row className={styles["scene"]} justify="center" align="middle">
-      <Col id={styles['terrarium-container']}>
-        <img id={styles["terrarium-back"]} style={{ width: "100%" }} src='./mockup/petshop.png' />
+      <Col ref={sceneEl} id={styles['petshop-container']}>
+        <img data-depth={depths.back} id={styles["petshop-back"]} className={styles["petshop-scene"]} src='./mockup/petshop-back.png' />
+        <img data-depth={depths.counter} id={styles["petshop-counter"]} className={styles["petshop-scene"]} src='./mockup/petshop-counter.png' />
+        <img data-depth={depths.register} id={styles["petshop-register"]} className={styles["petshop-scene"]} src='./mockup/petshop-register.png' />
+        <div id={styles["parallax-hermit"]} className={styles["petshop-scene"]} data-depth={depths.hermit}>
+          <div id={styles["hermit-container"]} >
+            <div id={styles["petshop-hermit"]} className={styles["petshop-scene"]}>
+              <img id={styles["hermit-flip"]} className={styles["petshop-scene"]} src='./mockup/petshop-hermit.png' />
+
+            </div>
+          </div>
+
+        </div>
+        <img data-depth={depths.terrarium} id={styles["petshop-terrarium"]} className={styles["petshop-scene"]} src='./mockup/petshop-terrarium.png' />
+        <img data-depth={depths.pedistal} id={styles["petshop-pedistal"]} className={styles["petshop-scene"]} src='./mockup/petshop-pedistal.png' />
+        <div id={styles["petshop-door-container"]} className={styles["petshop-scene"]} data-depth={depths.door}>
+          <img id={styles["petshop-door"]} src='./mockup/petshop-doorway.png' />
+
+        </div>
       </Col>
 
     </Row>
