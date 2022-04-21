bf3235948f772d28366db8565d2c171d59d306cd
gledinburgh <gledinburgh@gmail.com>
2022-04-21 12:42:14 -0500

    title restyle

--- Components/Layout/Header.tsx
+++ Components/Layout/Header.tsx
@@ -12,6 +12,7 @@ const Header = () => {
 
   return (
     <div id={styles["header-container"]}>
+      <div id={styles["nav-base"]}></div>
       <Row justify='start' align="bottom">
 
         <Col span={10} >
@@ -19,15 +20,16 @@ const Header = () => {
 
         </Col>
         <Col span={14}>
-          <Row justify='start' align="bottom" id={styles["nav"]}>
-            <Col span={8}>| <a>shop</a></Col>
-            <Col span={8}>| <a>about</a></Col>
-            <Col span={8}>| <a>instagram</a></Col>
+          <div className={styles["nav-wrapper"]}>
 
-
-          </Row>
-        </Col>
-      </Row>
+            <Row justify='start' align="bottom" id={styles["nav"]}>
+              <Col span={8}>| <a className={styles["link"]}>shop</a></Col>
+              <Col span={8}>| <a className={styles["link"]} > about</a></Col>
+              <Col span={8}>| <a className={styles["link"]} > instagram</a></Col>
+            </Row>
+          </div>
+        </Col >
+      </Row >
     </div >
   )
 }
